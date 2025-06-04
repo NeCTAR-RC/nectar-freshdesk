e0a57237dc8d15721c18a0a33ce48096f375a38a
Sam Morrison <sorrison@gmail.com>
2025-06-04 16:28:29 +1000

    Change gunicorn to run on 8080, fix access logs
    Change-Id: I2b4482e196bd3b0e3fce830bdb5126197efe8355

--- docker/docker-run-api.sh
+++ docker/docker-run-api.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ -f /vault/secrets/secret_envs ] && . /vault/secrets/secret_envs
 export PATH="/venv/bin:$PATH"
-gunicorn --bind 0.0.0.0:8613 --access-logfile=- --worker-tmp-dir /dev/shm --workers 2 nectar_freshdesk.wsgi:application
+gunicorn --bind :8080 --access-logfile=- --worker-tmp-dir /dev/shm --forwarded-allow-ips '*' --access-logformat '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' --workers 3 nectar_freshdesk.wsgi:application
